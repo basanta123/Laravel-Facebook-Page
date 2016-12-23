59ccc9d0115d534659aca894e1bbb17204d3dffd
Basanta <basantaoffice@gmail.com>
2016-12-23 22:27:00 +0545

    routing for landing page and call back url

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,33 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) {
+    $login_url = $fb->getLoginUrl(['email']);
+    return view('welcome')->with('login_url', $login_url);
 });
+
+Route::get('/page', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb)
+{
+    // Obtain an access token.
+    try {
+        $token = $fb->getAccessTokenFromRedirect();
+        if($token){
+        
+        Session::put('fb_user_access_token', (string) $token);
+        return redirect('pagevamp');
+
+        }
+
+    } catch (Facebook\Exceptions\FacebookSDKException $e) {
+        dd($e->getMessage());
+    }
+
+});
+
+Route::get('pagevamp', function() {
+$info = Session::get('fb_user_access_token');
+if($info){
+	echo 'Yes';
+}
+});  
+
