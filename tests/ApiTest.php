2eb56f2014f36807c6d36559bd574b7cf41b7520
Basanta Dhakal <basantaoffice@gmail.com>
2016-12-25 02:11:02 +0000

    Apply fixes from StyleCI

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -1,39 +1,29 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
 
 class ApiTest extends TestCase
 {
     /**
-     * testing the facebook page data
+     * testing the facebook page data.
      *
      * @return void
      */
     public function testPage()
     {
-       
         $response = $this->call('GET', '/api/v1/page');
-        
-        $this->assertEquals(200, $response->status());
 
+        $this->assertEquals(200, $response->status());
     }
 
-     /**
-     * testing the posts made by page
+    /**
+     * testing the posts made by page.
      *
      * @return void
      */
     public function testPosts()
     {
-       
         $response = $this->call('GET', '/api/v1/posts');
-        
-        $this->assertEquals(200, $response->status());
-       
-    }
 
-    
+        $this->assertEquals(200, $response->status());
+    }
 }
